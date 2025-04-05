a35517ab9ec41748f7f2bf8b4bc424751fee559b
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-04-04 19:22:34 -0600

    Cambios de escritura de archivos
    Cambios de escritura de archivos

--- ferreteria/cotizacion.php
+++ ferreteria/cotizacion.php
@@ -8,5 +8,16 @@
 
         echo "<h2>Gracias por tu solicitud de cotización, $nombre.</h2>";
         echo "<p>Hemos recibido tu solicitud de cotización y se te estará enviando a la brevedad posible al correo $correo</p>";
+
+        // Verifica si el directorio existe
+        (!is_dir('cotizaciones')) ? mkdir('cotizaciones') : '';
+        // Navegamos a la carpeta
+        chdir('cotizaciones');
+        // Creamos el archivo de cotización
+        $archivo = fopen('cotizaciones.txt','a+');
+        // Escribimos en el archivo
+        fwrite($archivo, "$nombre, $telefono, $correo, $descripcion \n");
+        fclose($archivo);
+        
     }
 ?>
