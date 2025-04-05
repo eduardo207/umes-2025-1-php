2d90eff09a1a1bf34824335f60e70afc466d2639
Jerson Eduardo Quiñonez <eduardo207@gmail.com>
2025-04-04 18:39:18 -0600

    Manipulación de directorios
    Manipulación de directorios

---
+++ directorios-archivos/index.php
@@ -0,0 +1,27 @@
+<?php
+    // Manipulación de directorios en PHP
+    if (is_dir('cotizaciones')){
+        echo "El directorio 'cotizaciones' ya existe.";
+    }else{
+        echo "El directorio 'cotizaciones' NO existe.";
+        mkdir('cotizaciones');
+        echo "<br /> El directorio 'cotizaciones' ha sido creado.";
+    }
+
+    // Creando carpeta de galeria
+    (!is_dir('galeria')) ?  mkdir('galeria'): '';
+
+    $archivos = scandir('galeria');
+    foreach($archivos as $archivo){
+        echo "<br />Archivo: " . $archivo;
+    }
+
+
+
+    // Path de directorio creado
+    chdir('cotizaciones');
+    echo "<br />El path del directorio creado es: " . getcwd();
+
+    
+
+?>
